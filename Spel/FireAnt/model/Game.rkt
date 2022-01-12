ce1bf46a0b8b1fa3bd01d621b2e651ed330e1533
SwiftyChicken <erwema25@gmail.com>
2022-01-12 19:37:46 +0100

    ADD: level folder for all level txt files

--- Spel/FireAnt/model/Game.rkt
+++ Spel/FireAnt/model/Game.rkt
@@ -1,7 +1,10 @@
+(#%require (only racket/base directory-list))
+
 (define (new-game)
   (let ((canvas (make-window 1600 1000 "Fire Ant")))
     (define (start)
-     ((canvas 'set-background!) "blue"))
+      (let ((levels (directory-list "level")))
+        (display levels)))
 
     (define (dispatch-game cmd)
       (cond ((eq? cmd 'start) start)))
