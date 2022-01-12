a54b9dfb1e5c8012364234115ecd6988dc3d4dfd
SwiftyChicken <erwema25@gmail.com>
2022-01-12 22:33:14 +0100

    FIX: racket and r5rs compatibility

--- Spel/FireAnt/model/Game.rkt
+++ Spel/FireAnt/model/Game.rkt
@@ -1,10 +1,13 @@
-(#%require (only racket/base directory-list))
+(#%require compatibility/mlist (only racket/base directory-list path->string))
 
 (define (new-game)
   (let ((canvas (make-window 1600 1000 "Fire Ant")))
     (define (start)
-      (let ((levels (directory-list "level")))
-        (display levels)))
+            ;; List all levels in level folder
+      (let ((levels (map path->string
+                         (list->mlist
+                           (directory-list "level")))))
+        (display (car levels))))
 
     (define (dispatch-game cmd)
       (cond ((eq? cmd 'start) start)))
