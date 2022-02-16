05040245ec3aae4196b9f744227fc9fd16a27f1c
SwiftyChicken <erwema25@gmail.com>
2022-02-16 12:59:16 +0100

    ADD: option to jump to next level

--- Spel/FireAnt/controller/Game.rkt
+++ Spel/FireAnt/controller/Game.rkt
@@ -1,3 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Game ADT is verantwoordelijk voor:
+;; [ ] Starten van juiste level
+;; [ ] Intializatie van de Player ADT
+;; [ ] Updaten van de modellen
+;; [ ] Updaten van de view
+;; [ ] Checken voor collisions met Player ADT
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (#%require compatibility/mlist (only racket/base
                                      directory-list
                                      path->string))
@@ -20,9 +28,22 @@
             (current-level (new-level player ;; Start with level 1
                                       (car levels))))
 
-        ((canvas 'set-update-callback!) ;; Game Loop
+        (define (next-level) ;; Update variables and return next level
+          (if (not (null? (cdr levels)))
+            (begin (set! levels (cdr levels))
+                   (set! current-level (new-level player 
+                                                  (car levels)))
+                   current-level)
+            (display "GAME COMPLETED")))
+
+        ((view 'game-loop)
          (lambda (ms)
-           ((current-level 'update))))))
+           (display current-level)
+           (newline)
+           (display levels)
+           (newline)
+           (display (next-level))
+           (newline)))))
 
     (define (dispatch cmd)
       (cond ((eq? cmd 'start) start)))
