df66b052b7637961f1e4b0de95e5a75c5ec19e84
SwiftyChicken <erwema25@gmail.com>
2022-02-20 18:00:00 +0100

    ADD: process pathing-code

--- Spel/FireAnt/model/Scorpion.rkt
+++ Spel/FireAnt/model/Scorpion.rkt
@@ -3,8 +3,23 @@
 ;; [ ] 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (new-scorpion position route)
-  (let ((type 'scorpion))
+(define (new-scorpion position unprocessed-path)
+  (define (code->path code) ;; Pathing Code consist of 2 digits followed by 1 letter
+    (if (not (eq? (modulo (length code) 3)
+                  0))
+      (error "Expected length of list to be divisible by 3" code))
+
+    (if (null? code)
+      '()
+      (let ((num (string->number (string (car code) 
+                                            (cadr code))))
+            (char (caddr code)))
+        (cons (cons num char) (code->path (cdddr code))))))
+
+  (let ((type 'scorpion)
+        (pathing (list->circular 
+                   (code->path unprocessed-path))))
+
 ;;;;;;;;;;;;;;;;;;; GETTERS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     (define (get-type)
       type)
@@ -20,4 +35,5 @@
             ((eq? cmd 'get-position) (apply get-position args))
             (else (error "Unkown command" cmd))))
 
+    (display pathing)
     dispatch))
